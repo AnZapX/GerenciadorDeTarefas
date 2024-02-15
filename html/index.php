f1132450dd3ebe72c756c887c44e80f3d7ff7574
Andreas Zapala <andreaszapala98@gmail.com>
2024-02-14 22:24:32 -0300

    Formulário(Parte HTML e CSS)

--- html/index.php
+++ html/index.php
@@ -1,3 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+        <h1>Gerenciador de Tarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_anem"></label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar Tarefa</button>
+            </form>
+        </div>
+            <div class="separator">
+            </div>
+            <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por Andreas Zapalá</p>
+        </div>
+    </div>
+    
 
-echo "Sou uma jamanta";
+</body>
+</html>
